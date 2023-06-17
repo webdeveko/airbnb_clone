eb1d55e84263fb8295cbfe2b5cb039339a33b8f7
Ekophe <genz.millenials@gmail.com>
2023-06-17 20:53:20 +0700

    first commit

---
+++ app/components/Heading.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+interface HeadingProps {
+    title: string;
+    subtitle?: string;
+    center?: boolean;
+}
+
+const Heading: React.FC<HeadingProps> = ({title, subtitle, center}) => {
+  return (
+    <div className={center? 'text-center' : 'text-start'}>
+        <div className="text-2xl font-bold">
+            {title}
+        </div>
+        <div className="">
+            {subtitle}
+        </div>
+    </div>
+  )
+}
+
+export default Heading
